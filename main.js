2b22a914ddd67750bd231070e679245c3f90ad85
yachen <io260188@gmail.com>
2020-07-02 22:16:00 +0800

    feat: details

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 Vue.component('component', {
     template: `
                 <div class="component">
-                    <button @click="updateData" class="updateButton">更新 component 數據</button>
-                    <h1>{{componentMsg}}</h1>
+                    <button @click="updateData" class="updateButton">點擊更新 component 數據</button>
+                    <p>{{componentMsg}}</p>
                 </div>
             `,
     data() {
@@ -24,19 +24,19 @@ Vue.component('component', {
     },
     beforeMount() {
         const h1Tag = document.querySelector('h1');
-        console.log('DOM 掛載之前，<h1> 為：', h1Tag); // DOM 掛載之前，<h1> 為：null
+        console.log('DOM 掛載之前，<p> 為：', h1Tag); // DOM 掛載之前，<p> 為：null
     },
     mounted() {
         const h1Tag = document.querySelector('h1');
-        console.log('DOM 掛載完成，<h1> 為：', h1Tag); // DOM 掛載完成，<h1> 為： <h1>​我是來自 component 的數據​</h1>​
+        console.log('DOM 掛載完成，<p> 為：', h1Tag); // DOM 掛載完成，<p> 為： <p>​我是來自 component 的數據​</p>​
     },
     beforeUpdate() {
         const h1Tag = document.querySelector('h1');
-        console.log('更新之前的 <h1> 內容為：', h1Tag.innerText); // 更新之前的 <h1> 內容為： 我是來自 component 的數據
+        console.log('更新之前的 <p> 內容為：', h1Tag.innerText); // 更新之前的 <p> 內容為： 我是來自 component 的數據
     },
     updated() {
         const h1Tag = document.querySelector('h1');
-        console.log('更新之後的 <h1> 內容為：', h1Tag.innerText); // 更新之後的 <h1> 內容為： 更新之後的 <h1> 內容為： component 的數據被更改了！
+        console.log('更新之後的 <p> 內容為：', h1Tag.innerText); // 更新之後的 <p> 內容為： 更新之後的 <p> 內容為： component 的數據被更改了！
     },
     beforeDestroy() {
         console.log('銷毀之前');
@@ -60,20 +60,20 @@ const vm = new Vue({
     },
     template: `
                 <div>
+                    <h1>在 console 中觀察二種 case 的 lifecycle</h1>
                     <section>
                         <h2>無 keep-alive：</h2>
-                        <button @click="toggleComponentWithOutKeepAlive" class="toggleComponentButton">無 keep-alive：<br>創建 / 銷毀 component</button>
+                        <button @click="toggleComponentWithOutKeepAlive" class="toggleComponentButton">點擊觸發<br>創建 / 銷毀 component</button>
                         <component v-if="isComponentWithOutKeepAliveDisplay"></component>
                     </section>
                     
                     <section>
                         <h2>有 keep-alive：</h2>
-                        <button @click="toggleComponentWithKeepAlive" class="toggleComponentButton">有 keep-alive：<br>激活 / 停用 component</button>
+                        <button @click="toggleComponentWithKeepAlive" class="toggleComponentButton">點擊觸發<br>激活 / 停用 component</button>
                         <keep-alive> 
                             <component v-if="isComponentWithKeepAliveDisplay"></component>
                         </keep-alive>
                     </section>
-                    <p>註：在 console 中觀察 lifecycle</p>
                 </div>
             `,
     methods: {
